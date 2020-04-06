1545e237a689fb17a27fc969cd92ef078e0f896a
Sreevenkat <gsvetr@gmail.com>
2020-04-06 09:20:14 +0530

    Ex 02 & 03(basic)

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,7 @@
 module Lib ( someFunc )where
 
 import qualified Zero.Server as Server
+import qualified Debug.Trace as Debug
 
 helloHandler :: Server.Handler
 helloHandler = 
@@ -10,8 +11,31 @@ helloCallback :: Server.Request -> Server.Response
 helloCallback req = 
     Server.stringResponse "hello"
 
+echoHandler :: Server.Handler
+echoHandler = Server.simpleHandler Server.POST "/echo" echoCallback
+
+echoCallback :: Server.Request -> Server.Response
+echoCallback req = 
+    Server.stringResponse (Server.requestBody req)
+
+getNumberWord :: String -> String
+getNumberWord num = 
+    case num of
+        "1" -> "one"
+        "2" -> "two"
+        "3" -> "three"
+        _ -> "whoops"
+
+caseMatchCallback :: Server.Request -> Server.Response
+caseMatchCallback req = 
+    Server.stringResponse (Debug.traceShowId (getNumberWord x))
+    where 
+        x = Server.requestBody req
+
 
 someFunc :: IO ()
-someFunc = Server.startServer [helloHandler]
+someFunc = Server.startServer [
+    helloHandler, echoHandler,
+    Server.simpleHandler Server.POST "/case" caseMatchCallback]
 
 

977d0646a77f84ca6dc09b65123add033c3fbf4c
Sreevenkat <gsvetr@gmail.com>
2020-04-14 07:01:40 +0530

    WIP

--- src/Lib.hs
+++ src/Lib.hs
@@ -18,15 +18,53 @@ echoCallback :: Server.Request -> Server.Response
 echoCallback req = 
     Server.stringResponse (Server.requestBody req)
 
+getDivisor :: Int -> Int
+getDivisor num = 
+    case num of
+        5 -> 10000
+        4 -> 1000
+        3 -> 100
+        2 -> 10
+        1 -> 1
+        _ -> num
+
+getTens :: Int -> String
+getTens numInt = 
+    case numInt of
+        10 -> "Ten"
+        9 -> "Nine"
+        8 -> "Eight"
+        7 -> "Seven"
+        6 -> "Six"
+        5 -> "Five"
+        4 -> "Four"
+        3 -> "Three"
+        2 -> "Two"
+        1 -> "One"
+
+getStr :: Int -> String
+getStr num = show num
+
+getLength :: Str -> Int
+getLength anyStr = length anyStr :: Int
+
+computeNumberWord :: Int -> Int -> String
+computNumberWord numInt numLen = 
+    if remainderLen > 1
+        then computeNumberWord (remainder remainderLen)
+        else getTens remainderStr
+    where
+        divisor = getDivisor numLen
+        remainder  = numInt % divisor
+        remainderStr = show remainder
+        remainderLen = length remainderStr :: Int
+
 getNumberWord :: String -> String
 getNumberWord numStr = 
-    case (Debug.traceShowId numInt) of
-        1 -> "one"
-        2 -> "two"
-        3 -> "three"
-        _ -> "whoops"
+    computeNumberWord (numInt numLen)
     where
         numInt = read numStr :: Int
+        numLen = length numStr :: Int
 
 caseMatchCallback :: Server.Request -> Server.Response
 caseMatchCallback req = 
